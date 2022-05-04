95ab725c6c34b8c7362803751afaf51e91e0f08a
echizenyayota <virtualmisso30@gmail.com>
2022-05-04 15:53:12 +0900

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import Table from './Table'
 
-class App extends React.Component {
+
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello, React!</h1>
+      <div className="container">
+        <Table />
       </div>
     )
   }
